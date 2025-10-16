3e471e6262b5b02afbeed7209850c4edf72cd934
riza569 <rizamuhammed569@gmail.com>
2025-10-16 20:24:37 +0530

    allowed host

--- shoppit/settings.py
+++ shoppit/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = "django-insecure-%q2)y3*k$!(0p&un8$*vy#+k9ac((=-+@r-zr!76(abznklm2p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["rizashop-api.onrender.com","localhost","127.0.0.1"]
 
 
 # Application definition
